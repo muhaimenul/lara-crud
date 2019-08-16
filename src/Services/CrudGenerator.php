4b7c3c68ddce0347c94bc7f384f98cd72d344530
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2019-08-16 21:28:12 +0600

    added controller generate method

--- src/Services/CrudGenerator.php
+++ src/Services/CrudGenerator.php
@@ -12,10 +12,72 @@ class CrudGenerator
 {
     protected $name;
 
-    public function __construct(string $name)
+    /**
+     * CrudGenerator constructor.
+     * @param string $name
+     */
+
+    public function __construct($name)
     {
         $this->name = $name;
     }
 
-    
+    /**
+     * generate crud related files
+     */
+
+    public function generate() {
+
+    }
+
+    /**
+     * get type related stubs
+     * @param $type
+     * @return string
+     */
+    public function getStub($type)
+    {
+        return file_get_contents(resource_path("stubs/$type.stub"));
+    }
+
+    /**
+     * generate controller
+     */
+
+    protected function controller() {
+        $name = $this->name;
+        $controllerTemplate = str_replace(
+            [
+                '{{modelName}}',
+                '{{modelNamePluralLowerCase}}',
+                '{{modelNameSingularLowerCase}}'
+            ],
+            [
+                $name,
+                strtolower(str_plural($name)),
+                strtolower($name)
+            ],
+            $this->getStub('Controller')
+        );
+        file_put_contents(app_path("/Http/Controllers/{$name}Controller.php"), $controllerTemplate);
+    }
+
+    protected function model() {
+
+    }
+
+    protected function request()
+    {
+
+    }
+
+    protected function migration()
+    {
+
+    }
+
+    protected function view(){
+
+    }
+
 }
