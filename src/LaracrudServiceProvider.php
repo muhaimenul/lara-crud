23dcefeafcbbfe47a6563bbc6c26abb13c053725
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2019-08-16 17:15:08 +0600

    package service provider

---
+++ src/LaracrudServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Muhaimenul\Laracrud;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaracrudServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
