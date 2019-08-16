6eef1ffe3a41b2325b70c56995e2afd9fd3233ee
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2019-08-17 01:48:02 +0600

    laracrudServiceprovider register

---
+++ src/Commands/GenerateCRUD.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Muhaimenul Islam
+ * Date: 8/17/2019
+ * Time: 12:12 AM
+ */
+namespace Muhaimenul\Laracrud\Commands;
+
+use Illuminate\Console\Command;
+use Muhaimenul\Laracrud\Services\CrudGenerator;
+
+class GenerateCRUD extends Command
+{
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:crud
+        {name : Class (singular) for example User}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate Crud including controller, model, views & migrations.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+        $crudGnt = new CrudGenerator($name);
+        $crudGnt->generate();
+    }
+
+}
--- src/LaracrudServiceProvider.php
+++ src/LaracrudServiceProvider.php
@@ -7,13 +7,33 @@ use Illuminate\Support\ServiceProvider;
 class LaracrudServiceProvider extends ServiceProvider
 {
     /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
      * Bootstrap services.
      *
      * @return void
      */
     public function boot()
     {
-
+//        $this->publishes([
+//            __DIR__ . '/../config/laracrud.php' => config_path('laracrud.php'),
+//        ]);
+//        $this->publishes([
+//            __DIR__ . '/../publish/views/' => base_path('resources/views/'),
+//        ]);
+//        if (\App::VERSION() <= '5.2') {
+//            $this->publishes([
+//                __DIR__ . '/../publish/css/app.css' => public_path('css/app.css'),
+//            ]);
+//        }
+        $this->publishes([
+            __DIR__ . '/stubs/' => base_path('resources/lara-crud/'),
+        ]);
     }
 
     /**
@@ -23,6 +43,8 @@ class LaracrudServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
+        $this->commands(
+            'Muhaimenul\Laracrud\Commands\GenerateCRUD'
+        );
     }
 }
