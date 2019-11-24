06a95ad3d29ebb5ed7f98a6aef2ef443a0e5c377
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2019-11-24 16:10:33 +0600

    changed base repository

--- src/Repositories/Repository.php
+++ src/Repositories/Repository.php
@@ -1,20 +1,19 @@
 <?php 
 
 namespace Muhaimenul\Laracrud\Repositories;
+
 abstract class Repository  {
+
     /**
      * @var $model \Illuminate\Database\Eloquent\Model
      */
 	protected $model ;
-	/**
-	 * Default Operator For Database Queries
-	 * @var string
-	 */
-	private $defaultOperator = '=';
+
 	/**
 	 * Abstract Method To Set The Model of Each Repository
 	 */
-	abstract protected function setModel() : string ;
+    abstract protected function setModel() : string ;
+    
 	/**
 	 * Takes setModel() Function And Crafts A New Model Instance
 	 */
@@ -23,276 +22,286 @@ abstract class Repository  {
 		$model = $this->setModel();
 		$this->model = new $model();
 	}
-	/**
-	 * 
-	 * Magic Methods
-	 *
-	 */
-	/**
-	 * To prevent function calls that does not exists
-	 * @param  Funcation Name
-	 * @param  Arguments,Can be an array or single variable
-	 * @return void
-	 */
-	public function __call($name,$arguments)
-    {
-    	return;
-    }
+
+
     /**
-     * To prevent overriding values from outside class/object
-     * @param Property of Repository ($this) Class
-     * @param Value
+     * @return mixed
      */
-    public function __set($property, $value) {
-    	return;
+    public function all()
+    {   $this->newInstance();
+        return $this->model->all();
     }
+
     /**
-     * @return Model instance
-     */
-	public function instance()
-	{
-		return $this->model;
-	}
-    /**
-     * Sets the default operator
-     * @param Operator
-     */
-    public function setDefaultOperator(string $operator = '=')
-    {
-        $this->defaultOperator = $operator;
-    }
-    /**
-     * @param  Attributes Array
-     * @return Model instance
-     */
-    public function create(array $data)
-    {
-        return $this->model->create($data);
-    }
-    /**
-     * @param  Array of arrays (of attributes)
-     * @return Boolean
-     */
-    public function createMultiple(array $data)
-    {
-        return $this->model->insert($data);
-    }
-    /**
-     * @param  primaryKey
-     * @return Model Instance
-     */
-    public function find(int $primaryKey)
-    {
-        return $this->model->find($primaryKey);
-    }
-    /**
-     * @param  $conds Array of conditions,can be an array of array
-     * @param  $select is the columns to be selected
-     * @return Model instance
-     */
-    public function findFirst(array $conds = [], $select = ['*'])
-    {
-        $this->model = $this->_get($conds);
-        return $this->model->first($select);
-    }
-    /**
-     * @param  $conds [Array of conditions,can be an array of array]
-     * @param  $select [is the columns to be selected]
-     * @param  $paginate [Pagination items per page]
-     * @return [Model instance]
-     */
-    public function getWhere(array $conds = [],$select = ['*'],int $paginate = 0)
-    {
-        $this->model = $this->_get($conds);
-        if($paginate > 0) {
-            return $this->model->select($select)->paginate($paginate);
-        }
-        return $this->model->select($select)->get();
-    }
-    /**
-     * Returns the number of rows (count) based on the condition
-     * @param  $conditions [Array or array of arrays]
-     * @return integer
-     */
-    public function getWhereCount(array $conds = [])
-    {
-        $this->model = $this->_get($conds);
-        return $this->model->count();
-    }
-    /**
-     * First row of the datatabse table
-     * @return Model Instance
+     * @return mixed
      */
     public function first()
     {
         return $this->model->first();
     }
+
     /**
-     * Last row of the datatabse table
-     * @return Model Instance
+     * @return mixed
      */
     public function last()
     {
         return $this->model->latest()->first();
     }
+
     /**
-     * Order by descend on column `created at`
-     * @return Collection
+     * @param array $data
+     * @return mixed
      */
-    public function latest()
+    public function create(array $data)
     {
-        return $this->model->latest();  
+        return $this->model->create($data);
     }
-	/**
-	 * @return Model Collection
-	 */
-	public function all()
-	{
-		return $this->model->all();
-	}
-	/**
-	 * @param  Atrributes
-	 * @param  primary key
-	 * @return Boolean
-	 */
-	public function update(array $data,int $id)
-	{
-		$instance = $this->find($id);
-		if($instance) {
-			return $instance->update($data);
-		}
-		// throw a generic error
-		return false;
-	}
-	/**
-	 * @param  primary key
-	 * @return boolean
-	 */
-	public function delete(int $primaryKey)
-	{
-		$instance = $this->find($primaryKey);
-		if($instance) {
-			return $instance->delete();
-		}
-		return false;
-	}
-	/**
-	 * @param  $conds is an array,can be a multi dimensional array (array of array)
-	 * @return Builder
-	 */
-	private function _get(array $conds) 
-	{
-		if(empty($conds)) {
-    		return false;
-    	}
-    	//check if $conds is array
-    	foreach($conds as $index => $cond ) {
-    		if(is_array($cond)) {
-	    		$column = $cond[0];
-	            if(isset($cond[2])) {
-	           		$operator = $cond[1];
-	           		$value    = $cond[2];
-	           	} else {
-	           		$operator = $this->defaultOperator; // default 
-	           		$value    = $cond[1];
-	           	}
-    		} else {
-    			$column = $conds[0];
-    			if(isset($conds[2])) {
-	           		$operator = $conds[1];
-	           		$value    = $conds[2];
-	           	} else {
-	           		$operator = $this->defaultOperator; // default
-	           		$value    = $conds[1];
-	           	}
-           	}
-           	$instance = $this->model->where($column, $operator, $value);
-    	}
-    	return $instance;
-	}
-	
+
     /**
-     * @param  $relation [string or array of relationshop]
-     * @return Builder
+     * @param array $data
+     * @return mixed
      */
-    public function with($relation)
+    public function createMultiple(array $data)
     {
-    	if(!is_array($relation)) {
-    		$relation = [$relation];
-    	}
-    	// callbackable relation
-    	return $this->model->with($relation);
+        return $this->model->insert($data);
+    }
+
+    /**
+     * @param array $condition
+     * @param array $data
+     * @return mixed
+     * @author Muhaimenul
+     */
+    public function createOrUpdate(array $condition, array $data)
+    {
+        return $this->model->updateOrCreate($condition, $data);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTrashed()
+    {
+        return $this->model->withTrashed();
+    }
+
+    /**
+     * @param $primaryKey
+     * @return mixed
+     */
+    public function find($primaryKey)
+    {   $this->newInstance();
+        return $this->model->find($primaryKey);
+    }
+
+    /**
+     * @param array $conditions
+     * @param array $select
+     * @return mixed
+     */
+    public function findWhere(array $conditions, $select = ['*'])
+    {
+        $this->newInstance();
+        foreach ($conditions as $key => $value ) {
+            if (is_array($value)) {
+                $this->model = $this->model->where($value[0], $value[1], $value[2]);
+            }else{
+                $this->model = $this->model->where($key, $value);
+            }
+        }
+        return $this->model->select($select);
+    }
+
+    public function findWhereFirst(array $data)
+    {
+        return $this->model->where($data)->first();
+    }
+
+    /**
+     * @param array $data
+     * @param int $primaryKey
+     * @return mixed
+     */
+    public function update(int $primaryKey,array $data)
+    {
+        return $this->model->find($primaryKey)->update($data);
+    }
+
+    /**
+     * @param array $data
+     * @param array $conditions
+     * @return mixed
+     */
+    public function updateByFields(array $conditions, array $data)
+    {
+        foreach($conditions as $key => $value) {
+            $this->model = $this->model->where($key, $value);
+        }
+        return $this->model->update($data);
+    }
+
+    /**
+     * @param $column
+     * @param $value
+     * @return mixed
+     */
+    public function where($column, $value)
+    {
+        if(is_array($column)) {
+            return $this->model->where($column[0],$column[1],$value)->get();
+        }
+        return $this->model->where($column,$value)->get();
+    }
+
+    /**
+     * @param array $conds
+     * @param array $select
+     * @return mixed
+     */
+    public function getWhere($conds = [], $select = ['*'])
+    {
+        foreach ($conds as $key=>$val) {
+            if (is_array($val)) {
+                $data = $this->model->where($val[0], $val[1], $val[2]);
+            }else{
+                $data = $this->model->where($key, $val);
+            }
+        }
+        $data = $this->model->get($select);
+        return $data;
+    }
+
+    /**
+     * @param $relationship
+     * @return $this
+     */
+    public function with($relationship)
+    {
+        $this->model = $this->model->with($relationship);
+        return $this;
+    }
+    public function withQuery($relation)
+    {
+        $this->model = $this->model->with($relation);
+        return $this;
+    }
+
+    /**
+     * @param $primaryKey
+     * @return mixed
+     */
+    public function delete($primaryKey)
+    {
+        return $this->model->delete($primaryKey);
+    }
+
+    /**
+     * @param array $conditions
+     * @return mixed
+     */
+    public function findFirst(array $conditions)
+    {
+        $data = $this->model->where($conditions)->first();
+        return $data;
+    }
+
+    /**
+     * @param array $conditions
+     * @return $this
+     */
+    public function deleteWhere(array $conditions)
+    {
+        $this->findWhere($conditions)->delete();
+        return $this;
+    }
+
+    /**
+     * @param $primaryKey
+     * @return $this
+     */
+    public function exists($primaryKey)
+    {
+        $this->find($primaryKey)->exists();
+        return $this;
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function getInsertedId(array $data)
+    {
+        return $this->model->insertGetId($data);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInstance()
+    {
+        return $this->model;
+    }
+
+    /**
+     * @param int $itemPerPage
+     * @return mixed
+     */
+    public function paginate(int $itemPerPage = 20)
+    {
+        return $this->model
+            ->paginate($itemPerPage);
+    }
+
+    /**
+     * @param string $column
+     * @param string $value
+     * @return mixed
+     */
+    public function searchByColumn(string $column, string $value)
+    {
+        return $this->model
+            ->where($column ,'LIKE','%' . $value . '%')
+            ->get();
+    }
+
+    /**
+     * @param string $column
+     * @param string $value
+     * @return mixed
+     */
+    public function searchWhere(string $column, string $value)
+    {
+        return $this->model
+            ->where($column , $value )
+            ->first();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrimaryKey()
+    {
+        return $this->model->getKeyName();
+    }
+
+    /**
+     * The method utilizes laravel's ->where([[array1],[array2]..[arrayn]])
+     * where clause as laravel does not ship with and where in this version
+     * 5.7.x
+     * $multipleConditions is an array or array
+     * The function returns the Where instance,so the developer should chain
+     * ->first(),last(),get() ..etc laravel eloquent's methods.
+     *
+     * @return $model instance
+     */
+    public function whereWithMultiple(array $multipleConditions)
+    {
+        return $this->model->where($multipleConditions)->get();
     }
     
-    // needs testing
-    /**
-     * @param  $conds [Array of conditions]
-     * @param  $callback [Callback function]
-     * @param  $select [columns to be selected]
-     * @param  $paginate [Pagination items per page]
-     * @return Collection
-     */	
-    public function getFiltered(array $conds = [],$callback,$select = ['*'],int $paginate = 0)
+    public function newInstance()
     {
-    	$collection = $this->getWhere($conds,$select);
-    	$filtered = $collection->filter($callback);
-    	if($paginate > 0) {
-    		return $filtered->paginate($paginate);
-    	}
-    	return $filtered->all();
-    }
-    /**
-     * @param  $conds [Array or array of array,conditions]
-     * @param  $list [Array or single list item to be checked]
-     * @param  $select [Columns to be selected]
-     * @return Collection
-     */
-    public function whereHas(array $conds = [],$list,$select =['*'])
-    {
-    	$collection = $this->getWhere($conds,$select);
-    	return $collection->has($list);
-     }
-     /**
-      * @param  $conds [Array or array of array,conditions]
-      * @param  $place [Position of the elemnet to be picked]
-      * @param  $select [Columns to be selected]
-      * @return Model instance
-      */
-    public function whereNth(array $conds = [],$place,$select = ['*'])
-    {
-    	$collection = $this->getWhere($conds,$select);
-    	return $collection->nth($place);
-    }
-    /**
-     * @param  $conds [Array or array of array,conditions]
-     * @param  $list [Array or single list item to be checked]
-     * @param  $select [Columns to be selected]
-     * @param  $paginate [Pagination items per page]
-     * @return A filtered collection
-     */
-    public function getWhereOnly(array $conds,$list,$select = ['*'],$paginate = 0)
-    {
-    	$collection = $this->getWhere($conds,$select);
-    	$filtered = $collection->only($list);
-    	if($paginate > 0) {
-    		return $filtered->paginate($paginate);
-    	}
-    	return $filtered->all();
-    }
-    /**
-     * @param  $column [Sorting Column]
-     * @param  $oder [Order by]
-     * @param  $paginate [Pagination items per page]
-     * @return Collection
-     */
-    public function getFormatted($column = 'created_at',$order = 'ASC',$paginate = 0)
-    {
-    	$instance = $this->model->orderBy($column,$order);
-    	
-    	if($paginate > 0) {
-    		return $instance->paginate($paginate);
-    	} else {
-    		return $instance->get();
-    	}
+        $model = $this->setModel();
+        return $this->model =  new $model();
     }
 }
