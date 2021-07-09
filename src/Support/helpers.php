27850dfee95eff929a53cade8fd0f3c9c5da9fe5
muhaimenul <muhaimenulislam@iut-dhaka.edu>
2021-07-09 19:04:10 +0600

    init helper files

---
+++ src/Support/helpers.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by Muhaimenul.
+ * User: Muhaimenul
+ * Date: 6/17/2021
+ * Time: 9:51 PM
+ */
+
+if(! function_exists('_auth')) {
+    function _auth() {
+        return auth();
+    }
+}
+
+if(! function_exists('_auth_user')) {
+    function _auth_user() {
+        return _auth()->user();
+    }
+}
+
+if(! function_exists('is_authenticated')) {
+    function is_authenticated() {
+        return _auth()->check();
+    }
+}
+
+if(! function_exists('getUniqueCode')) {
+    function getUniqueCode()
+    {
+        return md5(microtime() . uniqid(rand(), true)) . time();
+    }
+}
+
+if(!function_exists('isUrl')) {
+    function isUrl($url) {
+        if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
+            return false;
+        }
+        return true;
+    }
+}
+
+if(!function_exists('string_plural')) {
+    function string_plural($string) {
+        \Illuminate\Support\Str::plural($string);
+    }
+}
